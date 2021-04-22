1066433023d04e8daeea6e756b78e4d96a0c4cd3
totallyNotAUser <68023215+totallyNotAUser@users.noreply.github.com>
2021-04-22 14:42:40 +0300

    Make stage 2 a separate file (part 2)

--- cvm-arch.sh
+++ cvm-arch.sh
@@ -60,39 +60,7 @@ echo [*] Generating fstab
 genfstab -U /mnt >> /mnt/etc/fstab
 
 echo [*] Entering stage 2
-cat > /mnt/stage2.sh << EOF
-echo [***] Stage 2 started [***]
-
-echo [*] Setting UTC timezone
-ln -sf /usr/share/zoneinfo/UTC /etc/localtime
-hwclock --systohc
-
-echo [*] Setting locale
-echo 'en_US.UTF-8 UTF-8' >> /etc/locale.gen
-locale-gen
-echo 'LANG=en_US.UTF-8' >> /etc/locale.conf
-
-echo [*] Setting hostname
-echo "vm" >> /etc/hostname
-echo "127.0.0.1    localhost" >> /etc/hosts
-echo "::1        localhost" >> /etc/hosts
-echo "127.0.1.1    vm.localdomain vm" >> /etc/hosts
-
-echo [*] Deleting root account password
-passwd -d root
-
-echo [*] Creating user account
-useradd -m -G wheel -s /bin/bash user
-passwd -d user
-echo "%wheel ALL=(ALL) NOPASSWD:ALL" >> /etc/sudoers
-
-echo [*] Installing a bootloader called GRUB, the Grand Unified one
-grub-install --target=i386-pc ${diskdev}
-grub-mkconfig -o /boot/grub/grub.cfg
-
-echo [*] Enabling services
-systemctl enable dhcpcd
-EOF
+curl https://raw.githubusercontent.com/totallyNotAUser/cvm-arch/main/stage2.sh -o /mnt/stage2.sh
 chmod +x /mnt/stage2.sh
 arch-chroot /mnt /stage2.sh
 
